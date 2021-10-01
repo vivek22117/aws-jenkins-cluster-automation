d52606283f382fa826977bd60f552e0b7fdaf9d7
vivek22117 <vivekmishra22117@gmail.com>
2021-10-01 11:46:46 +0530

    adding TF resources for Jenkins EFS

---
+++ aws-eks-tf-modules/module.jenkins-efs-cluster/data.tf
@@ -0,0 +1,12 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.environment}-jenkins-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key     = "state/${var.environment}/vpc/terraform.tfstate"
+    region  = var.default_region
+  }
+}
+
+
+data "aws_caller_identity" "current" {}
