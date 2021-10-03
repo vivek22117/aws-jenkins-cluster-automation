047268c5030eb699c9b827aa7846a6d6c0cac0cd
vivek22117 <vivekmishra22117@gmail.com>
2021-10-03 12:33:46 +0530

    adding jenkins master script

--- aws-jenkins-tf-modules/module.jenkins-master/jenkins-master.tf
+++ aws-jenkins-tf-modules/module.jenkins-master/jenkins-master.tf
@@ -1,46 +1,120 @@
-resource "aws_key_pair" "jenkins_master" {
-  key_name   = "jenkins-key"
-  public_key = var.public_key
+########################################################
+#    Key pair to be used for SSH access                #
+########################################################
+resource "tls_private_key" "jenkins_host_ssh_data" {
+  algorithm = "RSA"
 }
 
-resource "aws_launch_configuration" "jenkins_master" {
-  # Launch Configurations cannot be updated after creation with the AWS API.
-  # In order to update a Launch Configuration, Terraform will destroy the
-  # existing resource and create a replacement.
-  #  # We're only setting the name_prefix here,
-  # Terraform will add a random string at the end to keep it unique.
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "jenkins-admin-key"
+  public_key = tls_private_key.jenkins_host_ssh_data.public_key_openssh
 
-  name_prefix          = "jenkins-"
-  image_id             = data.aws_ami.jenkins-master-ami.id
-  instance_type        = var.environment == "qa" ? "t2.small" : var.instance_type
-  iam_instance_profile = aws_iam_instance_profile.jenkins_profile.id
-  key_name             = aws_key_pair.jenkins_master.key_name
-  security_groups      = [aws_security_group.jenkins_master_sg.id]
+  tags = merge(local.common_tags, map("Name", "jenkins-nodes-ssh-key"))
+}
 
-  user_data = data.template_file.script.rendered
+resource "aws_launch_template" "jenkins_master_lt" {
+  name_prefix = "${var.component_name}-lt-${var.environment}"
 
-  associate_public_ip_address = false
-  enable_monitoring           = false
+  update_default_version = true
+  image_id      = data.aws_ami.jenkins-master-ami.id
+  instance_type = local.instance_types[0]
+  key_name      = aws_key_pair.ssh_key.key_name
 
-  spot_price = var.spot_price
+  user_data = base64encode(data.template_file.script.rendered)
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.jenkins_profile.arn
+  }
+
+  network_interfaces {
+    device_index                = 0
+    associate_public_ip_address = false
+    security_groups = [aws_security_group.jenkins_master_sg.id]
+    delete_on_termination = true
+  }
+
+  placement {
+    tenancy = "default"
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size           = var.volume_size
+      volume_type           = "gp2"
+      delete_on_termination = true
+    }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(local.common_tags, map("Project", "${var.component_name}-cluster"))
+  }
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_autoscaling_group" "jenkins_master_asg" {
-  depends_on = [aws_lb.jenkins_alb]
 
-  name_prefix         = "${aws_launch_configuration.jenkins_master.name}-asg"
+
+resource "aws_autoscaling_group" "jenkins_master_asg" {
+  depends_on = [aws_alb.jenkins_alb]
+
+  name_prefix         = "${var.component_name}-jenkins-master-asg-${var.environment}"
 
   vpc_zone_identifier  = data.terraform_remote_state.vpc.outputs.private_subnets
-  max_size             = 3
-  min_size             = var.environment == "prod" ? 2 : 1
-  desired_capacity     = var.environment == "prod" ? 2 : 1
-  launch_configuration = aws_launch_configuration.jenkins_master.id
+
+  capacity_rebalance  = true
+
+  termination_policies      = var.termination_policies
+  max_size             = var.app_asg_max_size
+  min_size             = var.app_asg_min_size
+  desired_capacity     = var.app_asg_desired_capacity
   target_group_arns = [aws_lb_target_group.jenkins_target_group.arn]
-  health_check_type    = "ELB"
+  health_check_grace_period = var.app_asg_health_check_grace_period
+  health_check_type         = var.health_check_type
+  wait_for_elb_capacity     = var.app_asg_wait_for_elb_capacity
+  wait_for_capacity_timeout = var.wait_for_capacity_timeout
+
+  default_cooldown = var.default_cooldown
+  suspended_processes = var.suspended_processes
+
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.jenkins_master_lt.id
+        version            = aws_launch_template.jenkins_master_lt.latest_version
+      }
+      dynamic "override" {
+        for_each = local.instance_types
+        content {
+          instance_type     = override.value
+          weighted_capacity = "1"
+        }
+      }
+    }
+    instances_distribution {
+      on_demand_base_capacity                  = 0
+      on_demand_percentage_above_base_capacity = 0
+      spot_allocation_strategy                 = "capacity-optimized"
+      spot_max_price                           = module.spot-price.spot_price_current_max
+      #spot_max_price                           = module.spot-price.spot_price_current_optimal
+      #spot_max_price                           = module.spot-price.spot_price_current_min
+    }
+  }
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      instance_warmup        = 0 #demo only, 120
+      min_healthy_percentage = 0 #demo only, 90
+    }
+  }
 
   lifecycle {
     create_before_destroy = true
@@ -56,21 +130,22 @@ resource "aws_autoscaling_group" "jenkins_master_asg" {
   }
 }
 
-resource "aws_lb" "jenkins_alb" {
-  name = "jenkins-cluster-alb"
+resource "aws_alb" "jenkins_alb" {
+  name = "${var.component_name}-alb"
 
+  load_balancer_type = "application"
   subnets                   = data.terraform_remote_state.vpc.outputs.public_subnets
   security_groups           = [aws_security_group.lb_sg.id]
   internal                  = false
   enable_http2              = "true"
-  idle_timeout              = 300
+  idle_timeout              = 600
 
-  tags = merge(local.common_tags, map("Name", "jenkins-master-lb"))
+  tags = merge(local.common_tags, map("Name", "${var.component_name}-${var.environment}-lb"))
 }
 
 
 resource "aws_lb_listener" "jenkins_alb_listener" {
-  load_balancer_arn = aws_lb.jenkins_alb.arn
+  load_balancer_arn = aws_alb.jenkins_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -80,8 +155,26 @@ resource "aws_lb_listener" "jenkins_alb_listener" {
   }
 }
 
+resource "aws_alb_listener_rule" "ecs_alb_listener_rule" {
+  depends_on = [aws_lb_target_group.jenkins_target_group]
+
+  listener_arn = aws_lb_listener.jenkins_alb_listener.arn
+  priority     = "100"
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.jenkins_target_group.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+}
+
 resource "aws_lb_target_group" "jenkins_target_group" {
-  name = "jenkins-tg-${var.environment}"
+  name = "${var.component_name}-tg-${var.environment}"
 
   port        = var.default_target_group_port
   protocol    = "HTTP"
@@ -89,7 +182,7 @@ resource "aws_lb_target_group" "jenkins_target_group" {
   target_type = "instance"
 
   tags = {
-    name = "jenkins-tg"
+    name = "${var.component_name}-tg"
   }
 
   health_check {
