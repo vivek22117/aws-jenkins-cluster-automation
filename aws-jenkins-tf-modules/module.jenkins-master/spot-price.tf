30abd1c12581c1499779597a5da9c7634e8fbc60
vivek22117 <vivekmishra22117@gmail.com>
2021-10-03 12:39:51 +0530

    adding jenkins master script

--- aws-jenkins-tf-modules/module.jenkins-master/spot-price.tf
+++ aws-jenkins-tf-modules/module.jenkins-master/spot-price.tf
@@ -26,4 +26,21 @@ data "aws_ec2_spot_price" "this" {
 
 locals {
   price_per_unit_map = { for item in data.aws_ec2_spot_price.this : "${item.availability_zone}/${item.instance_type}" => tonumber(item.spot_price) / tonumber(lookup(local.azs_instances_weights, "${item.availability_zone}/${item.instance_type}", var.instance_weight_default)) }
+
+  price_per_unit_list = values(local.price_per_unit_map)
+  price_current_optimal = max([
+  for az in local.azs_list : min([
+  for key in keys(local.price_per_unit_map) : lookup(local.price_per_unit_map, key) if split("/", key)[0] == az
+  ]...)
+  ]...)
+  price_current_min = min(local.price_per_unit_list...)
+  price_current_max = max(local.price_per_unit_list...)
+
+  spot_price_current_max = ceil(tonumber(format("%f", local.price_current_max)) * var.normalization_modifier) / var.normalization_modifier
+  spot_price_current_max_mod = ceil(tonumber(format("%f", local.price_current_max)) * var.custom_price_modifier * var.normalization_modifier) / var.normalization_modifier
+  spot_price_current_min = ceil(tonumber(format("%f", local.price_current_min)) * var.normalization_modifier) / var.normalization_modifier
+  spot_price_current_min_mod = ceil(tonumber(format("%f", local.price_current_min)) * var.custom_price_modifier * var.normalization_modifier) / var.normalization_modifier
+  spot_price_current_optimal = ceil(tonumber(format("%f", local.price_current_optimal)) * var.normalization_modifier) / var.normalization_modifier
+  spot_price_current_optimal_mod = ceil(tonumber(format("%f", local.price_current_optimal)) * var.custom_price_modifier * var.normalization_modifier) / var.normalization_modifier
+
 }
