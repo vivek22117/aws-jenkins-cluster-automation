7f452e40fa6b8b36edc782299920a35cd01c5ebb
vivek22117 <vivekmishra22117@gmail.com>
2021-10-05 17:10:44 +0530

    updating AMI filter type

---
+++ aws-jenkins-tf-modules/module.jenkins-slaves/data_sources.tf
@@ -0,0 +1,24 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.environment}-jenkins-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key     = "state/${var.environment}/vpc/terraform.tfstate"
+    region  = var.default_region
+  }
+}
+
+
+#####================Jenkins Master state file==================#####
+data "terraform_remote_state" "jenkins_master" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.environment}-jenkins-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key     = "state/${var.environment}/jenkins-master/terraform.tfstate"
+    region  = var.default_region
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
