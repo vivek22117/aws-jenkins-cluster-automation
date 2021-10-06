d9801bc13592a42c3600774a7849146d41e3c6f6
vivek22117 <vivekmishra22117@gmail.com>
2021-10-06 09:17:35 +0530

    adding test config

---
+++ deployment/jenkins-efs-cluster/main.tf
@@ -0,0 +1,13 @@
+####################################################
+#        EFS Cluster module implementation         #
+####################################################
+module "jenkins_efs_cluster" {
+  source = "../../aws-jenkins-tf-modules/module.jenkins-efs-cluster"
+
+  environment = var.environment
+
+  efs_lifecycle = var.efs_lifecycle
+  isEncrypted = var.isEncrypted
+  performance_mode = var.performance_mode
+  throughput_mode = var.throughput_mode
+}
---
+++ deployment/jenkins-efs-cluster/output.tf
@@ -0,0 +1,7 @@
+output "efs_id" {
+  value = module.jenkins_efs_cluster.efs_id
+}
+
+output "efs_dns" {
+  value = module.jenkins_efs_cluster.efs_dns
+}
