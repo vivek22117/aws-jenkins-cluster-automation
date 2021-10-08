6ef1c0ad58bcc4115be0894ff989fba228f0a532
vivek22117 <vivekmishra22117@gmail.com>
2021-10-08 10:00:32 +0530

    jenkins cluster module implementation

--- deployment/jenkins-cluster/out.tf
+++ deployment/jenkins-cluster/out.tf
@@ -21,3 +21,34 @@ output "jenkins_profile" {
 output "route53_public_dns_name" {
   value = module.jenkins_master_cluster.route53_public_dns_name
 }
+
+#####=========================================================================================#####
+output "spot_price_current_max" {
+  description = "Maximum current Spot Price, which allows to run all Instance Types in all AZ. Maximum stability."
+  value       = module.jenkins_master_cluster.spot_price_current_max
+}
+
+output "spot_price_current_max_mod" {
+  description = "Modified maximum current Spot Price. (multiplied by the `custom_price_modifier`). Additional stability on rare runs of terraform apply."
+  value       = module.jenkins_master_cluster.spot_price_current_max_mod
+}
+
+output "spot_price_current_min" {
+  description = "Minimum current Spot Price, which allows to run at least one Instance Type in at least one AZ. Lowest price."
+  value       = module.jenkins_master_cluster.spot_price_current_min
+}
+
+output "spot_price_current_min_mod" {
+  description = "Modified minimum current Spot Price. (multiplied by the `custom_price_modifier`). Additional stability on rare runs of terraform apply."
+  value       = module.jenkins_master_cluster.spot_price_current_min_mod
+}
+
+output "spot_price_current_optimal" {
+  description = "Optimal current Spot Price, which allows to run at least one Instance Type in all AZ. Balance between stability and costs."
+  value       = module.jenkins_master_cluster.spot_price_current_optimal
+}
+
+output "spot_price_current_optimal_mod" {
+  description = "Modified optimal current Spot Price. (multiplied by the `custom_price_modifier`). Additional stability on rare runs of terraform apply."
+  value       = module.jenkins_master_cluster.spot_price_current_optimal_mod
+}
