77dc8e2b6ec2a4e982a95d8d42e5670a545be70a
vivek22117 <vivekmishra22117@gmail.com>
2021-11-28 10:41:06 +0530

    adding new variable volume type

--- aws-jenkins-vpc-tf-modules/module.vpc-endpoints/data.tf
+++ aws-jenkins-vpc-tf-modules/module.vpc-endpoints/data.tf
@@ -5,8 +5,8 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket  = "${var.environment}-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
-    key     = "state/${var.environment}/vpc/terraform.tfstate"
+    bucket  = "${var.environment}-jenkins-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key     = "state/${var.environment}/jenkins-vpc/terraform.tfstate"
     region  = var.default_region
   }
 }
