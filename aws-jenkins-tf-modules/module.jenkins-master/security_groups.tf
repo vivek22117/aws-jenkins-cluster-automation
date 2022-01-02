440056d3558f5be5fbee3f8fbba787d931ef8d64
vivek22117 <vivekmishra22117@gmail.com>
2022-01-02 22:28:25 +0530

    updating sg for jenkins master

--- aws-jenkins-tf-modules/module.jenkins-master/security_groups.tf
+++ aws-jenkins-tf-modules/module.jenkins-master/security_groups.tf
@@ -17,12 +17,12 @@ resource "aws_security_group_rule" "allow_traffic_from_lb" {
 }
 
 resource "aws_security_group_rule" "allow_ssh_traffic" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.jenkins_master_sg.id
-  source_security_group_id = data.terraform_remote_state.vpc.outputs.bastion_sg
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.jenkins_master_sg.id
+  cidr_blocks       = [data.terraform_remote_state.vpc.outputs.vpc_cidr]
 }
 
 resource "aws_security_group_rule" "master_outbound_rule" {
