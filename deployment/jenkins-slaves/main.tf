b88af811895271233601c5059747b8fe686ac2be
vivek22117 <vivekmishra22117@gmail.com>
2022-01-02 12:10:41 +0530

    adding TF script to implement jenkins slaves module

--- deployment/jenkins-slaves/main.tf
+++ deployment/jenkins-slaves/main.tf
@@ -1,17 +1,29 @@
 ####################################################
 #        Jenkins Slaves module implementation      #
 ####################################################
-module "jenkins_efs_cluster" {
+module "jenkins_slave" {
   source = "../../aws-jenkins-tf-modules/module.jenkins-slaves"
 
   environment = var.environment
 
-  instance_count = 0
-  instance_type = ""
-  jenkins_credentials_id = ""
-  jenkins_password = ""
-  jenkins_username = ""
-  max_count = 0
-  profile = ""
-  spot_price = ""
+  instance_count = var.instance_count
+  instance_type = var.instance_type
+  jenkins_credentials_id = var.jenkins_credentials_id
+  jenkins_password = var.jenkins_password
+  jenkins_username = var.jenkins_username
+  max_count = var.max_count
+  spot_price = var.spot_price
+  app_asg_desired_capacity = var.app_asg_desired_capacity
+  app_asg_health_check_grace_period = var.app_asg_health_check_grace_period
+  app_asg_max_size = var.app_asg_max_size
+  app_asg_min_size = var.app_asg_min_size
+  app_asg_wait_for_elb_capacity = var.app_asg_wait_for_elb_capacity
+  component_name = var.component_name
+  default_cooldown = var.default_cooldown
+  health_check_type = var.health_check_type
+  suspended_processes = var.suspended_processes
+  termination_policies = var.termination_policies
+  volume_size = var.volume_size
+  volume_type = var.volume_type
+  wait_for_capacity_timeout = var.wait_for_capacity_timeout
 }
