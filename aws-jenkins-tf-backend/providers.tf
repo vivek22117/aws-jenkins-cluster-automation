efb3cb5fe6342b652cdd975fe5de9d89abe8e77a
vivek22117 <vivekmishra22117@gmail.com>
2022-01-05 18:51:41 +0530

    adding new variable volume type

--- aws-jenkins-tf-backend/providers.tf
+++ aws-jenkins-tf-backend/providers.tf
@@ -13,11 +13,11 @@ terraform {
 
   required_version = ">= 0.13"
 
-  backend "s3" {
-    #    profile        = "default"
-    region  = "us-east-1"
-    encrypt = "true"
-  }
+//  backend "s3" {
+//    #    profile        = "default"
+//    region  = "us-east-1"
+//    encrypt = "true"
+//  }
 }
 
 data "aws_caller_identity" "current" {}
